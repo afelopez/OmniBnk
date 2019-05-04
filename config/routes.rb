5370c3aa34ec6d03b736da35127e68cabe193dfd
Andrés F. López <ing.ratosocial@gmail.com>
2019-05-04 15:08:18 -0500

    jwt init

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   constraints subdomain: 'api' do
-    
+    resources :users, param: :_username
+    post '/auth/login', to: 'authentication#login'
+    get '/*a', to: 'application#not_found'
   end
 end
